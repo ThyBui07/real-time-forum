f76ac32e891396affc5e729b99cf11b6ee1a3800
ThyBui07 <buithinguyenthy@gmail.com>
2023-05-16 16:22:40 +0300

    ws

--- websocket/wsmanager.go
+++ websocket/wsmanager.go
@@ -3,6 +3,7 @@ package websocket
 import (
 	"log"
 	"net/http"
+	"sync"
 
 	"github.com/gorilla/websocket"
 )
@@ -19,11 +20,22 @@ var (
 
 // Manager is used to hold references to all Clients Registered, and Broadcasting etc
 type Manager struct {
+	clients ClientList
+
+	// Using a syncMutex here to be able to lcok state before editing clients
+	// Could also use Channels to block
+	sync.RWMutex
+	// handlers are functions that are used to handle Events
+	handlers map[string]EventHandler
+	// otps is a map of allowed OTP to accept connections from
+	// otps RetentionMap
 }
 
 // NewManager is used to initalize all the values inside the manager
 func NewManager() *Manager {
-	return &Manager{}
+	return &Manager{
+		clients: make(ClientList),
+	}
 }
 
 // serveWS is a HTTP Handler that the has the Manager that allows connections
@@ -36,6 +48,34 @@ func (m *Manager) ServeWS(w http.ResponseWriter, r *http.Request) {
 		log.Println(err)
 		return
 	}
-	// We wont do anything yet so close connection again
-	conn.Close()
+	// Create New Client
+	client := NewClient(conn, m)
+	// Add the newly created client to the manager
+	m.AddClient(client)
+}
+
+// addClient will add clients to our clientList
+func (m *Manager) AddClient(client *Client) {
+	// Lock so we can manipulate
+	m.Lock()
+	defer m.Unlock()
+
+	// Add Client
+	m.clients[client] = true
+	//start go routine readmess
+	go client.ReadMessages()
+}
+
+// removeClient will remove the client and clean up
+func (m *Manager) RemoveClient(client *Client) {
+	m.Lock()
+	defer m.Unlock()
+
+	// Check if Client exists, then delete it
+	if _, ok := m.clients[client]; ok {
+		// close connection
+		client.connection.Close()
+		// remove
+		delete(m.clients, client)
+	}
 }
