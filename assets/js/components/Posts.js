1fb90b76623094d30f0884de329814cc368184b2
ThyBui07 <buithinguyenthy@gmail.com>
2023-05-16 16:24:02 +0300

    js components

---
+++ assets/js/components/Posts.js
@@ -0,0 +1,34 @@
+import Observer from "../lib/Observer.js";
+console.log('posts.js')
+class Posts extends Observer {
+  createMarkup(state) {
+    const posts = JSON.parse(state.posts);
+    return posts.map(post => `
+    <div class="card flex-md-row mb-4 h-md-250">
+      <div class="card-body d-flex flex-column align-items-start">
+        <h3 class="mb-0">
+          <a class="text-dark" href="#">${post.Title}</a>
+        </h3>
+        <div class="mb-1 text-muted">${post.Date}</div>
+        <p class="card-text mb-auto">${post.Content}</p>
+        <a href="#">Continue reading</a>
+      </div>
+    </div>
+  `).join('')
+  }
+
+  render(state, selector = "app") {
+    const markup = this.createMarkup(state);
+    const parent = document.getElementById(selector);
+
+    parent.innerHTML = markup;
+  }
+
+  // This method will be called by the Subject(state) whenever it updates.
+  // Notice how it prompts a re-render.
+  update(state) {
+    this.render(state, "posts-container");
+  }
+}
+
+export default Posts;
