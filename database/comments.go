307cced03619ab42f8d85e067e5b8a43c3b375f9
ThyBui07 <buithinguyenthy@gmail.com>
2023-05-17 17:16:09 +0300

    fix table,upd func:add commentCount to postTable

--- database/comments.go
+++ database/comments.go
@@ -12,9 +12,11 @@ type Comment struct {
 func CreateCommentsTable(db *sql.DB) {
 	commentsTable := `CREATE TABLE IF NOT EXISTS Comments (
 		ID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+		UserID INTEGER NOT NULL,
 		Username STRING NOT NULL,
 		PostID INTEGER NOT NULL,
 		Content TEXT NOT NULL,
+		FOREIGN KEY(UserID) REFERENCES Users(UserID) ON DELETE CASCADE,
 		FOREIGN KEY(Username) REFERENCES Users(Username) ON DELETE CASCADE,
 		FOREIGN KEY(PostID) REFERENCES Posts(ID) ON DELETE CASCADE);`
 
@@ -24,16 +26,23 @@ func CreateCommentsTable(db *sql.DB) {
 	fmt.Println("Comments table created successfully!")
 }
 
-func AddComment(db *sql.DB, username string, postID int, content string) error {
+func AddComment(db *sql.DB, userID int, username string, postID int, content string) error {
 	// Prepare the SQL statement to insert the comment
-	statement, err := db.Prepare("INSERT INTO Comments (Username, PostID, Content) VALUES (?, ?, ?)")
+	statement, err := db.Prepare("INSERT INTO Comments (UserID, Username, PostID, Content) VALUES (?, ?, ?, ?)")
 	if err != nil {
 		return err
 	}
 	defer statement.Close()
 
 	// Execute the SQL statement with the provided parameters
-	_, err = statement.Exec(username, postID, content)
+	_, err = statement.Exec(userID, username, postID, content)
+	if err != nil {
+		return err
+	}
+
+	// Update the CommentCount in the Posts table
+	updateQuery := "UPDATE Posts SET CommentCount = CommentCount + 1 WHERE PostID = ?"
+	_, err = db.Exec(updateQuery, postID)
 	if err != nil {
 		return err
 	}
